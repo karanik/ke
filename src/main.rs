8b41900b5bd80290a6dd3119d5c7e0e7024d895d
Kostas Karanikolas <karanikolas@gmail.com>
2022-05-21 14:46:08 +0300

    Refactored loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{event, terminal};
+use crossterm::{cursor, event, execute, terminal};
+use crossterm::terminal::ClearType; 
+use anyhow::Result;
 use std::time::Duration;
+use std::io::stdout;
 
 struct CleanUp;
 impl Drop for CleanUp {
@@ -9,26 +12,70 @@ impl Drop for CleanUp {
     }
 }
 
-fn main() -> crossterm::Result<()> {
+fn clear_screen() -> crossterm::Result<()> {
+    execute!(stdout(), terminal::Clear(ClearType::All));
+    execute!(stdout(), cursor::MoveTo(0, 0))
+}
+
+
+struct Editor {
+    exit : bool
+}
+
+impl Editor {
+    pub fn new() ->Result<Editor> {
+        Ok(Editor {
+            exit: false
+        })
+    }
+
+    pub fn on_key_event(&mut self, kevent : KeyEvent) -> Result<()> {
+        match kevent {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+            } => self.exit = true,
+            _ => { }
+        }
+        Ok(())
+    }
+    pub fn on_idle(&mut self) {
+        println!("No input yet\r");
+    }
+
+    pub fn run_loop(&mut self) ->  Result<()> {
+        loop {
+            if event::poll(Duration::from_millis(500))? /* modify */ {
+                let event = event::read()?;
+                
+                match event {
+                    Event::Key(kevent) => {
+                        self.on_key_event(kevent)?;
+                    },
+                    _ => {
+                    }
+                };
+    
+                if self.exit {
+                    break;
+                } else {
+                    println!("{:?}\r", event);
+                }
+            } else {
+                self.on_idle()
+            }
+        }
+        Ok(())
+    }
+}
+
+
+
+
+fn main() -> anyhow::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?; /* modify */
-    loop {
-        if event::poll(Duration::from_millis(500))? /* modify */ {
-            if let Event::Key(event) = event::read()? /* modify */ { 
-                match event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: event::KeyModifiers::NONE,
-                    } => break,
-                    _ => {
-                        //todo
-                    }
-                }
-                println!("{:?}\r", event);
-            };
-        } else {
-            println!("No input yet\r");
-        }
-    }
+    let mut editor = Editor::new()?;
+    editor.run_loop()?;
     Ok(())
 }
