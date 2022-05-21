461055c1564ebc5089d65adf8e7a90c8894dd467
Kostas Karanikolas <karanikolas@gmail.com>
2022-05-21 19:59:26 +0300

    Basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
 use crossterm::event::{Event, KeyCode, KeyEvent};
 use crossterm::terminal::ClearType;
-use crossterm::{cursor, event, execute, terminal};
+use crossterm::{cursor, event, execute, queue, terminal};
+use std::io::prelude::*;
 use std::io::stdout;
 use std::str::FromStr;
 use std::time::Duration;
-use std::io::prelude::*;
 
 struct CleanUp;
 impl Drop for CleanUp {
@@ -14,9 +14,10 @@ impl Drop for CleanUp {
     }
 }
 
+#[derive(Copy, Clone)]
 struct Point {
-    pub x: u32,
-    pub y: u32,
+    x: usize,
+    y: usize,
 }
 
 struct EditBuffer {
@@ -38,14 +39,27 @@ impl EditBuffer {
         }
         Ok(())
     }
+
+    pub fn num_lines(&self) -> usize {
+        self.lines.len()
+    }
+}
+
+struct Rect {
+    pos : Point,
+    size : Point
+}
+
+impl Rect {
+    pub fn new () -> Rect {
+        Rect { pos: Point { x:0, y:0 } , size: Point { x: 0, y: 0 } }
+    }
 }
 
 struct Editor {
     exit: bool,
-    width: u16,
-    height: u16,
+    view: Rect,
     buffer: EditBuffer,
-    viewOffset: Point,
     cursor: Point,
 }
 
@@ -53,65 +67,112 @@ impl Editor {
     pub fn new() -> Result<Editor> {
         Ok(Editor {
             exit: false,
-            width: 0,
-            height: 0,
+            view : Rect::new(),
             buffer: EditBuffer::new(),
-            viewOffset: Point { x: 0, y: 0 },
             cursor: Point { x: 0, y: 0 },
         })
     }
 
     pub fn on_key_event(&mut self, kevent: KeyEvent) -> Result<()> {
+        let mut redraw = false;
+        let mut new_cursor = self.cursor;
         match kevent {
             KeyEvent {
                 code: KeyCode::Char('c'),
                 modifiers: event::KeyModifiers::CONTROL,
             } => self.exit = true,
+            KeyEvent {
+                code: KeyCode::Down,
+                modifiers: event::KeyModifiers::NONE,
+            } => {
+                if self.cursor.y < self.buffer.num_lines() {
+                    self.cursor.y = self.cursor.y + 1;
+                    redraw = true;
+                }
+            }
+            KeyEvent {
+                code: KeyCode::Up,
+                modifiers: event::KeyModifiers::NONE,
+            } => {
+                if self.cursor.y > 0 {
+                    self.cursor.y = self.cursor.y - 1;
+                    redraw = true;
+                }
+            }
             _ => {}
         }
+
+        if self.cursor.y > (self.view.pos.y + self.view.size.y - 1) {
+            self.view.pos.y = self.cursor.y - self.view.size.y + 1;
+        } else if self.cursor.y < self.view.pos.y  {
+            self.view.pos.y = self.cursor.y;
+        }
+
+        if redraw {
+            self.draw()?
+        }
+
         Ok(())
     }
     pub fn on_idle(&mut self) {
         //  println!("No input yet\r");
     }
 
-    pub fn on_resize(&mut self, new_width: u16, new_height: u16) {
-        self.width = new_width;
-        self.height = new_height;
+    pub fn on_resize(&mut self, new_width: usize, new_height: usize) -> Result<()> {
+        self.view.size.x = new_width;
+        self.view.size.y = new_height;
+        self.draw()?;
+        Ok(())
     }
 
-    //fn buffer_to_view(bufferPoint : Point) -> Option<Point> {
-    //}
+//    fn buffer_to_view(&self, bufferPoint : Point) -> (i32, i32) {
+ //       i32 x = self
+  //  }
+
+//    fn 
 
     fn draw(&self) -> Result<()> {
-        // Clear screen
-        execute!(stdout(), terminal::Clear(ClearType::All))?;
-        execute!(stdout(), cursor::MoveTo(0, 0))?;
+        let mut out = String::new();
 
-        for y in 0..self.height {
-            let buffer_y = (self.viewOffset.y + y as u32) as usize;
-            let bline = if buffer_y < self.buffer.lines.len() {
+        // Clear screen
+        queue!(stdout(), terminal::Clear(ClearType::All), cursor::MoveTo(0, 0))?;        
+
+        for y in 0..self.view.size.y {
+            let buffer_y = self.view.pos.y + y;
+            let bline = if buffer_y < self.buffer.num_lines() {
                 self.buffer.lines[buffer_y].clone()
             } else {
                 String::new()
             };
-            for x in 0..self.width {
-                execute!(stdout(), cursor::MoveTo(x, y))?;
-                let buffer_x = (self.viewOffset.x + x as u32) as usize;
+
+            for x in 0..self.view.size.x{
+                let buffer_x = self.view.pos.x + x;
                 let outc = if buffer_x < bline.len() {
                     bline.chars().nth(buffer_x).unwrap()
                 } else {
                     ' '
                 };
-                print!("{}", outc);
+                //execute!(stdout(), cursor::MoveTo(x as u16, y as u16))?;
+                //print!("{}", outc);
+                out.push(outc);
             }
         }
 
+        write!(stdout(), "{}", out)?;
         // Draw status
-        execute!(stdout(), cursor::MoveTo(0, self.height - 1))?;
-        print!("Status bar");
-
-        execute!(stdout(), cursor::MoveTo(0, 0))?;
+        //queue!(stdout(), cursor::MoveTo(0, (self.view.size.y - 1) as u16))?;
+        //write!(stdout(), "Status bar")?;
+       
+        queue!(stdout(), cursor::MoveTo(0, 0))?;
+        write!(stdout(), "Pos({},{}) View({},{}) Size({},{})", 
+            self.cursor.x, self.cursor.y,
+            self.view.pos.x, self.view.pos.y,
+            self.view.size.x, self.view.size.y
+        )?;
+       
+        
+        queue!(stdout(), cursor::MoveTo((self.cursor.x - self.view.pos.x) as u16, (self.cursor.y - self.view.pos.y) as u16))?;
+        stdout().flush()?;
         Ok(())
     }
 
@@ -120,8 +181,7 @@ impl Editor {
         //println!("{:?}\r", new_size);
         //return Ok(());
 
-        self.on_resize(new_size.0, new_size.1);
-        self.draw()?;
+        self.on_resize(new_size.0 as usize, new_size.1 as usize)?;
         loop {
             if !event::poll(Duration::from_millis(500))? {
                 self.on_idle()
@@ -131,13 +191,16 @@ impl Editor {
                     Event::Key(kevent) => {
                         self.on_key_event(kevent)?;
                     }
+                    Event::Resize(x, y) => {
+                        self.on_resize(x as usize, y as usize)?;
+                    }
                     _ => {}
                 };
 
                 if self.exit {
                     break;
                 } else {
-                    println!("{:?}\r", event);
+                    //println!("{:?}\r", event);
                 }
             }
         }
